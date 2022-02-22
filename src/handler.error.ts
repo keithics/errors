bec558091123f053d4faca6104719b6741a25c17
keithics <keith@webninjamobile.com>
2022-02-22 10:47:47 +0800

    error will always return a valid http status code

--- src/handler.error.ts
+++ src/handler.error.ts
@@ -29,14 +29,21 @@ function errorHandler(err, req, res, next) {
     next(err);
   }
 
-  const statusCode = err.code || 500;
+  const validStatusCode = [
+    100, 101, 200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401,
+    402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502,
+    503, 504, 505,
+  ];
+
+  const statusCode = validStatusCode.includes((err?.code || 500)) && err.hasOwnProperty('code')  ? err.code : 500;
   const message: ArkErrorInterface = err.message || 'An error occurred';
   res.status(statusCode);
 
   // different errors and statuses
   if (statusCode < 500) return res.jsonp({ message, error: true });
   if (statusCode === 11000) return res.status(422).jsonp(getUniqueErrorMessage(err));
-  if (err.name === 'JsonWebTokenError') return res.status(401).jsonp({ message: 'Invalid JWT', error: true });
+  if (err.name === 'JsonWebTokenError')
+    return res.status(401).jsonp({ message: 'Invalid JWT', error: true });
   Sentry.captureException(message);
   return res.jsonp({ error: true, statusCode, message });
 }
